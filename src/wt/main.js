2569e104f0e6cbac1ecd93f44a19715912b49310
jijek-com <mas.eug@yandex.ru>
2025-10-27 02:36:54 +0500

    feat: added wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+import path from 'node:path';
+
 const performCalculations = async () => {
-  // Write your code here
+    const cpuCount = os.cpus().length;
+    const promises = [];
+
+    for (let i = 0; i < cpuCount; i++) {
+        const worker = new Worker(path.resolve('./src/wt/worker.js'));
+
+        const promise = new Promise((resolve) => {
+            worker.on('message', (msg) => resolve(msg));
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+            worker.on('exit', (code) => {
+                if (code !== 0) resolve({ status: 'error', data: null });
+            });
+
+            worker.postMessage(10 + i);
+        });
+
+        promises.push(promise);
+    }
+
+    const results = await Promise.all(promises);
+    console.log(results);
 };
 
 await performCalculations();
